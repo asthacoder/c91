88d026190e7934e90088bccdf3a3d101a0b7e971
Astha <70913929+asthacoder@users.noreply.github.com>
2021-07-10 17:48:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import * as React from "react";
+import { createSwitchNavigator, createAppContainer } from "react-navigation";
+
+import LoginScreen from "./screens/LoginScreen";
+import LoadingScreen from "./screens/LoadingScreen";
+import DashboardScreen from "./screens/LogoutScreen";
+
+import * as firebase from "firebase";
+import { firebaseConfig } from "./config";
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app();
+}
+
+const AppSwitchNavigator = createSwitchNavigator({
+ 
+  LoadingScreen: LoadingScreen,
+     LoginScreen: LoginScreen,
+  LogoutScreen:  LogoutScreen
+});
+
+const AppNavigator = createAppContainer(AppSwitchNavigator);
+
+export default function App() {
+  return <AppNavigator/>;
+}
